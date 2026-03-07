8a68288b10fb7fce4040dc8358946cb4b6ef298f
Kalindu Rashmina <krashmina612@gmail.com>
2026-03-07 18:34:14 +0530

    Minor Changes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 import httpx
 from datetime import datetime, timezone
+import json
 
 from app.config import Settings, load_produce_standards
 from app.models import BlockchainHistoryResponse, InsightsResponse, SummaryOut, TransportOut, TempHumidityRange, DataQualityOut, ProofOut
@@ -29,7 +30,10 @@ async def get_insights(batch_id: str, include_raw: bool = False):
             if r.status_code == 404:
                 raise HTTPException(status_code=404, detail="Batch not found in blockchain history API")
             r.raise_for_status()
-            payload = r.json()
+            payload = json.loads(r)[0]
+            # payload = payload_list[0]
+            # payload = '{"txId":"935480d9f8d9c2a8e2536288ac385052ed2ee5413bbcd009fc67f798a435ec5c","blockTimestamp":{"seconds":1772441464,"nanos":100000000},"isDelete":false,"data":{"batchID":"BATCH-234560","farmerId":"OF12345678","harvestedDate":"2026-02-28T16:14:33.000Z","organicLevel":"95","plantedDate":"2025-12-01T08:20:14.000Z","status":"DELIVERED","produceType":"Organic Cabbage","farmerName":"Saman Fernando","supplierId":"Sup-003","transporterId":"ad4f9780-814f-4867-a388-fd786d0447ef","pickupLocation":"Embilipitiya Farm","weightKg":"1000.5","invoiceHash":"QmdKnPUaT1ppK5g6Km6tXgeYjBboDGNbXVQ7NB36V26Ho2","notes":"Handled with care","pickupTimeStamp":"2026-02-28T04:38:38.000Z","deliveryTimestamp":"2026-03-01T08:51:04.000Z","syncTimestamp":"2026-03-01T08:51:04.000Z","minTemp":24.5,"maxTemp":28.2,"avgTemp":26,"minHumidity":51,"maxHumidity":61,"avgHumidity":55.5,"merkleRoot":"8f434346648f6b96df89dda901c5176b10a6d83961dd3c1ac88b59b2dc327aa4"},"blockTimestampLK":"03/01/2026, 02:21:04 PM"}'
+            # payload_json = json.loads(payload)
     except httpx.TimeoutException:
         raise HTTPException(status_code=504, detail="Blockchain API timed out")
     except httpx.HTTPError as e:
